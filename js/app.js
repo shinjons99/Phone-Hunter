70e4d234929a3076478759f1a3f4c0e34dc68d69
shinjons99 <shinjons99@gmail.com>
2023-02-26 22:53:04 +0600

    Toggle no phone found message based on search result

--- js/app.js
+++ js/app.js
@@ -8,6 +8,18 @@ const loadPhones = async(searchText) =>{
 const displayPhone = phones =>{
    const phonesContainer = document.getElementById('phone-container');
    phonesContainer.textContent = '';
+   //* display 20 phones only
+   phones = phones.slice(0, 10);
+   
+    //* display no phone found
+    const noPhone = document.getElementById('no-found-message');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none')
+    }
+    else{
+        noPhone.classList.add('d-none');
+    }
+    //* display all phones
    phones.forEach(phone =>{
     const phoneDiv = document.createElement('div');
     phoneDiv.classList.add('col');
@@ -33,4 +45,4 @@ document.getElementById('btn-search').addEventListener('click', function(){
     loadPhones(searchText);
 })
 
-loadPhones();
+// loadPhones();
