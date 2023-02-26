5dad9f3a3655800638da88194e3b1afc786408fc
shinjons99 <shinjons99@gmail.com>
2023-02-26 23:14:19 +0600

    Show and hide loading spinner while loading API data

--- js/app.js
+++ js/app.js
@@ -33,16 +33,31 @@ const displayPhone = phones =>{
       </div>
     </div>
   </div>
-    
-    `
+    `;
     phonesContainer.appendChild(phoneDiv);
-   })
+   });
+   //* stop spinner loader
+   toggleSpinner(false);
 }
 
+//* handle search button click
 document.getElementById('btn-search').addEventListener('click', function(){
+    //* start loader
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhones(searchText);
 })
+
+const toggleSpinner = isLoading =>{
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
 
 // loadPhones();
