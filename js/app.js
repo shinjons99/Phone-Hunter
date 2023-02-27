3dca084b0341a6730204d502b4bffbf89dab0062
shinjons99 <shinjons99@gmail.com>
2023-02-27 20:33:25 +0600

    Dynamic API Data Load and search on keyboard enter button

--- js/app.js
+++ js/app.js
@@ -36,8 +36,9 @@ const displayPhone = (phones, dataLimit) =>{
     <div class="card p-4">
       <img src="${phone.image}" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">${phone.phone_name}</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
       </div>
     </div>
   </div>
@@ -64,6 +65,14 @@ document.getElementById('btn-search').addEventListener('click', function(){
     processSearch(10);
 })
 
+//* search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function(e){
+    
+    if(e.key === 'Enter'){
+        processSearch(10);
+    }
+})
+
 const toggleSpinner = isLoading =>{
     const loaderSection = document.getElementById('loader');
     if(isLoading){
@@ -77,5 +86,12 @@ const toggleSpinner = isLoading =>{
 document.getElementById('btn-show-all').addEventListener('click', function(){
     processSearch();
 })
+
+const loadPhoneDetails = async id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+}
 
 // loadPhones();
